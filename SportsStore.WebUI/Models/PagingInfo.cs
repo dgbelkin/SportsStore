bf507af2f545926fecdfad2392c682fdcbe6e250
dan48475 <d.g.belkin@gmail.com>
2016-01-17 18:55:03 +0500

    99

--- SportsStore.WebUI/Models/PagingInfo.cs
+++ SportsStore.WebUI/Models/PagingInfo.cs
@@ -11,7 +11,7 @@ namespace SportsStore.WebUI.Models
         public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
 
-        public int TotalPages => (int) Math.Ceiling((decimal) TotalItems/ItemsPerPage );
+        public int TotalPages => (int) Math.Ceiling((decimal) TotalItems/ItemsPerPage  );
 
     }
 }
